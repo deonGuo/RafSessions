b4c82661113b461346e3c0182adb4453d4a5a7a9
DeonGuo <deon.guo@gmail.com>
2020-04-12 23:55:55 +0100

    Initial draft 1. count down works 2. timer dispays 3. announcement of exercises
    to do:
    1. hardcoded exercises
    2. random encouragement

--- frmMain.Designer.cs
+++ frmMain.Designer.cs
@@ -31,16 +31,17 @@
             this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtProgram = new System.Windows.Forms.TextBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.label1 = new System.Windows.Forms.Label();
-            this.lblInitialization = new System.Windows.Forms.Label();
+            this.lblTimer = new System.Windows.Forms.Label();
             this.txtDebug = new System.Windows.Forms.TextBox();
+            this.lblMinCountDown = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(1549, 54);
+            this.button1.Location = new System.Drawing.Point(50, 197);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(290, 83);
             this.button1.TabIndex = 0;
@@ -50,62 +51,80 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(1549, 271);
+            this.button2.Location = new System.Drawing.Point(50, 397);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(290, 83);
             this.button2.TabIndex = 1;
-            this.button2.Text = "Start";
+            this.button2.Text = "Pause";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // textBox1
+            // txtProgram
             // 
-            this.textBox1.Location = new System.Drawing.Point(412, 54);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(1033, 300);
-            this.textBox1.TabIndex = 2;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.txtProgram.Location = new System.Drawing.Point(505, 55);
+            this.txtProgram.Multiline = true;
+            this.txtProgram.Name = "txtProgram";
+            this.txtProgram.Size = new System.Drawing.Size(1033, 1003);
+            this.txtProgram.TabIndex = 2;
+            this.txtProgram.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 400;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(1520, 460);
+            this.label1.Location = new System.Drawing.Point(1799, 472);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(100, 37);
             this.label1.TabIndex = 3;
             this.label1.Text = "label1";
             // 
-            // lblInitialization
+            // lblTimer
             // 
-            this.lblInitialization.AutoSize = true;
-            this.lblInitialization.Location = new System.Drawing.Point(32, 57);
-            this.lblInitialization.Name = "lblInitialization";
-            this.lblInitialization.Size = new System.Drawing.Size(184, 37);
-            this.lblInitialization.TabIndex = 4;
-            this.lblInitialization.Text = "Initialization";
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTimer.Location = new System.Drawing.Point(74, 55);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(220, 82);
+            this.lblTimer.TabIndex = 4;
+            this.lblTimer.Text = "Timer";
             // 
             // txtDebug
             // 
-            this.txtDebug.Location = new System.Drawing.Point(412, 460);
+            this.txtDebug.Location = new System.Drawing.Point(1582, 742);
             this.txtDebug.Multiline = true;
             this.txtDebug.Name = "txtDebug";
-            this.txtDebug.Size = new System.Drawing.Size(1033, 300);
+            this.txtDebug.Size = new System.Drawing.Size(591, 300);
             this.txtDebug.TabIndex = 5;
+            // 
+            // lblMinCountDown
+            // 
+            this.lblMinCountDown.AutoSize = true;
+            this.lblMinCountDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMinCountDown.Location = new System.Drawing.Point(1607, 81);
+            this.lblMinCountDown.Name = "lblMinCountDown";
+            this.lblMinCountDown.Size = new System.Drawing.Size(457, 326);
+            this.lblMinCountDown.TabIndex = 6;
+            this.lblMinCountDown.Text = "60";
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(2243, 893);
+            this.ClientSize = new System.Drawing.Size(2243, 1141);
+            this.Controls.Add(this.lblMinCountDown);
             this.Controls.Add(this.txtDebug);
-            this.Controls.Add(this.lblInitialization);
+            this.Controls.Add(this.lblTimer);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtProgram);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "frmMain";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmMain_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -115,11 +134,12 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtProgram;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label lblInitialization;
+        private System.Windows.Forms.Label lblTimer;
         private System.Windows.Forms.TextBox txtDebug;
+        private System.Windows.Forms.Label lblMinCountDown;
     }
 }
 
--- frmMain.cs
+++ frmMain.cs
@@ -14,12 +14,44 @@ namespace RafSessions
     public partial class frmMain : Form
     {
         SpeechSynthesizer speech;
+        int h, m, s, s60, itemNumber;
+        List<string> program;
         public frmMain()
         {
             InitializeComponent();
+            program = new List<string>{
+                                "Get Ready.  Starting in 1 minute from now",
+                                "Single Leg Box						",
+                                "one and half Bottomed Out Squats   ",
+                                "Jump Squats                        ",
+                                "Handstand Pushups                  ",
+                                "Rotational Pushups                 ",
+                                "Cobra Pushups                      ",
+                                "Single Leg Heel Touch Squats       ",
+                                "Sprinter Lunges                    ",
+                                "Jump Sprinter Lunges               ",
+                                "Pullups OR Push ups                ",
+                                "Body weight Sliding Pulldowns      ",
+                                "Inverted Chin Curls or Push ups    ",
+                                "Reverse Corkscrews                 ",
+                                "Black Widow Knee Slides            ",
+                                "Levitation Crunches                ",
+                                "Angels and Devils                  "
+              };
+            //set the initial value of nextitem to be the first entry of the program
             this.speech = new SpeechSynthesizer();
             speech.SelectVoice("Microsoft David Desktop");
-
+            h = 0;
+            m = 0;
+            s = 0;
+            s60 = 60;
+            itemNumber = 0;
+            //set the program text box to display exercises
+            foreach(string exercise in program)
+            {
+                txtProgram.Text += exercise.Trim() + "\r\n";
+            }
+            //show installed voices
             txtDebug.Text = "Intalled voices: " + speech.GetInstalledVoices().Count + "\r\n";
             foreach (InstalledVoice voice in speech.GetInstalledVoices())
             {
@@ -31,7 +63,52 @@ namespace RafSessions
 
         private void button2_Click(object sender, EventArgs e)
         {
+            timer1.Stop();
+        }
 
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //setting the time increase
+            if (s == 60)
+            {
+                m++;
+                s = 0;
+            }
+            if (m == 60)
+            {
+                h++;
+                m = 0;
+            }
+            //s60 ticks down from 60 every minute
+            if (s60 == 0)
+            {
+                speech.SpeakAsync("NOW " + program[itemNumber]); //announces "NOW" instead of 0
+                s60 = 60;
+            }
+            //every minute when it reaches 10, it reads out the next line
+            if(s60 == 10)
+            {
+                itemNumber++;  //because each exercise is 60sec no break; move to next exercise every minute
+                speech.SpeakAsync("in 10 seconds: " + program[itemNumber]);
+                //nextItem = txtProgram.Text.Substring();
+                //speech.SpeakAsync();
+            }
+            if(s60 <= 5 && s60 > 0)  //count down for the last 5 seconds
+            {
+                speech.SpeakAsync(s60.ToString());
+            }
+            //display the time count down
+            lblTimer.Text = string.Format("{0} : {1} : {2}", h.ToString().PadLeft(2, '0'), m.ToString().PadLeft(2, '0'), s.ToString().PadLeft(2, '0'));
+            lblMinCountDown.Text = s60.ToString();
+
+            //All operations for this tick are completed.  Now move to next tick
+            s++;
+            s60--;
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -41,18 +118,8 @@ namespace RafSessions
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string speechContent = textBox1.Text;
-            label1.Text = speech.Voice.Id + ", " + speech.Voice.Description;
-
-            if(speechContent == "")
-            {
-                speech.SpeakAsync("I have nothing to say");
-            }
-            else
-            {
-                speech.SpeakAsync(speechContent);
-            }
-
+            timer1.Start();
+            speech.SpeakAsync(program[0]);
         }
     }
 }
