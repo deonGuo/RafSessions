04da693771858bad5f4b369a1cf6b202af913faa
DeonGuo <deon.guo@gmail.com>
2020-04-12 20:05:53 +0100

    Add project files.

---
+++ frmMain.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace RafSessions
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblInitialization = new System.Windows.Forms.Label();
+            this.txtDebug = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1549, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(290, 83);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1549, 271);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(290, 83);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(412, 54);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1033, 300);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1520, 460);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 37);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // lblInitialization
+            // 
+            this.lblInitialization.AutoSize = true;
+            this.lblInitialization.Location = new System.Drawing.Point(32, 57);
+            this.lblInitialization.Name = "lblInitialization";
+            this.lblInitialization.Size = new System.Drawing.Size(184, 37);
+            this.lblInitialization.TabIndex = 4;
+            this.lblInitialization.Text = "Initialization";
+            // 
+            // txtDebug
+            // 
+            this.txtDebug.Location = new System.Drawing.Point(412, 460);
+            this.txtDebug.Multiline = true;
+            this.txtDebug.Name = "txtDebug";
+            this.txtDebug.Size = new System.Drawing.Size(1033, 300);
+            this.txtDebug.TabIndex = 5;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2243, 893);
+            this.Controls.Add(this.txtDebug);
+            this.Controls.Add(this.lblInitialization);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblInitialization;
+        private System.Windows.Forms.TextBox txtDebug;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+
+namespace RafSessions
+{
+    public partial class frmMain : Form
+    {
+        SpeechSynthesizer speech;
+        public frmMain()
+        {
+            InitializeComponent();
+            this.speech = new SpeechSynthesizer();
+            speech.SelectVoice("Microsoft David Desktop");
+
+            txtDebug.Text = "Intalled voices: " + speech.GetInstalledVoices().Count + "\r\n";
+            foreach (InstalledVoice voice in speech.GetInstalledVoices())
+            {
+                VoiceInfo info = voice.VoiceInfo;
+                txtDebug.Text += ("Name: " + info.Name + "\r\n");
+            }
+            txtDebug.Text += "Selected Voice: " + speech.Voice.Name;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string speechContent = textBox1.Text;
+            label1.Text = speech.Voice.Id + ", " + speech.Voice.Description;
+
+            if(speechContent == "")
+            {
+                speech.SpeakAsync("I have nothing to say");
+            }
+            else
+            {
+                speech.SpeakAsync(speechContent);
+            }
+
+        }
+    }
+}
